21610f254a7f094f439d650045c94d8467ef83e3
Julián Felipe Bautista Vélez <julian.910606@gmail.com>
2020-03-03 22:19:18 +0100

    Upload de la aplicacion

--- app/globals.ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-export const url_post = "YOUR API URL TO POST";
-export const url_get = "YOUR API URL TO GET";
-export const query = "tabla";
-export const ingresos_stats = "ingresos_stats";
-export const gastos_stats = "gastos_stats";
-export const liquides_hist = "hist_dinero_stats";
